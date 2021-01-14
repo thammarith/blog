789b47d25222e0b42d5281da450e6a1793e05577
Thammarith <thammarith@users.noreply.github.com>
2021-01-14 22:12:13 +0700

    Cleanup

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,10 @@
 // custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
+// import "typeface-montserrat"
+// import "typeface-merriweather"
 // normalize CSS across browsers
-import "./src/normalize.css"
+// import "./src/normalize.css"
 // custom CSS styles
-import "./src/style.css"
+import './src/styles.scss';
 
 // Highlighting for code blocks
-import "prismjs/themes/prism.css"
+import 'prismjs/themes/prism.css';
--- src/components/layout.js
+++ src/components/layout.js
@@ -21,15 +21,18 @@ const Layout = ({ location, title, children }) => {
   }
 
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
-      <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
-      </footer>
-    </div>
+    <>
+      {children}
+    </>
+    // <div className="global-wrapper" data-is-root-path={isRootPath}>
+    //   <header className="global-header">{header}</header>
+    //   <main>{children}</main>
+    //   <footer>
+    //     © {new Date().getFullYear()}, Built with
+    //     {` `}
+    //     <a href="https://www.gatsbyjs.com">Gatsby</a>
+    //   </footer>
+    // </div>
   )
 }
 
